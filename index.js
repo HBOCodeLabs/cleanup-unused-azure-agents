6e58647f3a114347c615be398a110f3faf6387e5
Elliot Nelson <nelson.work@gmail.com>
2021-04-19 12:50:16 -0400

    add missing copyright banners

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Copyright (c) WarnerMedia Direct, LLC. All rights reserved. Licensed under the MIT license.
 // See the LICENSE file for license information.
+
 module.exports = require('./lib/cleanup-unused-agents');
